74ca579febc69f162c5cff41df03ae84e8e1047f
unknown <sebastien.amrhein@gmail.com>
2015-12-04 11:51:35 +0100

    finition de la vue

--- app/src/main/java/com/example/sebastien/bluechat/MainActivity.java
+++ app/src/main/java/com/example/sebastien/bluechat/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.sebastien.bluechat;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.accueil);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
